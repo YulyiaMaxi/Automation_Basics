53f39170529837195f709026237f0b97c2d19504
Netology <orjuice@mail.ru>
2023-06-19 20:57:41 +0300

    JUnit4 commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,5 +1,5 @@
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         public void shouldReturn0IfAmount1000() {
             int actual = service.remain(1000);
             int expected = 0;
-            Assert.assertEquals(actual, expected);
+            Assert.assertEquals(expected, actual);
         }
         @Test
         public void shouldReturn0IfAmount0() {
